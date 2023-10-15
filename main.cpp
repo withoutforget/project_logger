924c33a0c789ca059684a4535f10c309e187744c
withoutforget <89633210+withoutforget@users.noreply.github.com>
2023-10-15 14:50:43 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
 	try {
 		auto p = make_handler<basic_console_handler<std::u16string>>();
 		basic_logger<std::u16string> logger(p);
-		logger.log(u"Ïðèâåò ìèð");
+		logger.log(u"Привет мир");
 	}
 	catch (std::exception& e) {
 		std::cout << "exception:" << e.what() << std::endl;
